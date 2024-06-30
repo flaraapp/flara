094df096106568529753eda04f1db4260698fb04
sahith <78869400+sahith-p@users.noreply.github.com>
2024-06-30 13:11:07 -0700

    fix user

--- src/components/dashboard/UserMenu.tsx
+++ src/components/dashboard/UserMenu.tsx
@@ -8,9 +8,9 @@ interface UserMenuProps {
 export default function UserMenu({user}: UserMenuProps) {
 
   return (
-    <div className="absolute top-0 right-0 mt-4 mr-4 border-2 p-3 rounded-full shadow-md">
+    <div className="absolute top-0 right-0 mt-4 mr-4 border p-3 rounded-full shadow-md">
       <div className="flex items-center cursor-pointer">
-        <Image src={user.picture as string} alt={user.name as string} className="w-8 h-8 rounded-full mr-2" width={10} height={10}/>
+        <Image src={user.picture || "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"} alt={user.name as string} className="border w-8 h-8 rounded-full mr-2" width={50} height={50}/>
         <NavItem dropdown={true} name={user.nickname || user.name as string} childrenItems={[{name:"Logout", href: "/api/auth/logout"}, {name:"Home", href: "/home"}]}/>
       </div>
     </div>
