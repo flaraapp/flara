f9726bff4f513f74addf22378439c630de318d99
sahith <78869400+sahith-p@users.noreply.github.com>
2024-06-30 16:43:56 -0700

    changes to bar

--- src/components/dashboard/MenuBar.tsx
+++ src/components/dashboard/MenuBar.tsx
@@ -9,24 +9,22 @@ export default function MenuBar() {
     return (
         <motion.div
             className="absolute top-0 left-0 mt-4 ml-4 border p-3 rounded-full shadow-md sm:rounded-full sm:w-auto sm:flex sm:items-center z-20 bg-white"
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
             layout
             initial={{ borderRadius: 50 }}
             animate={{ borderRadius: isHovered ? 30 : 50 }}
             transition={{ type: "tween", duration: 0.1 }}
         >
-            <div className="cursor-pointer sm:hidden flex items-center p-2" onClick={() => setIsHovered(!isHovered)}>
+            <div className="cursor-pointer sm:hidden flex items-center p-[0.6rem]" onClick={() => setIsHovered(!isHovered)}>
                 <MenuButton
                     isOpen={isHovered}
-                    onClick={() => { }}
+                    onClick={() => {}}
                     strokeWidth="1.7"
                     height={12}
                     width={15}
                     lineProps={{ strokeLinecap: "round" }}
                     className={""}
                 />
-                {isHovered && <span className="ml-3 text-sm">Close</span>}
+                {isHovered && <span className="ml-5">Close</span>}
             </div>
             <div className="hidden sm:flex items-center space-x-4 mt-0">
                 <a href="/reports" className="flex items-center transition duration-500 hover:bg-gray-100 rounded-full p-2">
