c52a86c033b16b5fce612aa8f9dbb5e1960097c0
sahith <78869400+sahith-p@users.noreply.github.com>
2024-06-30 15:45:09 -0700

    reroute nf

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,7 +12,7 @@ export default function ErrorScreen({error}: ErrorProps) {
                 <span className='text-6xl text-gray-700 text-center pb-5'>404</span>
                 <span className='text-xl text-gray-500 text-center'>We couldn&apos;t find the page you were looking for.</span>
                 <div className='h-full flex mb-5 items-center w-full justify-center'>
-                    <Button text='Return home' href='/home' type='primary'></Button>
+                    <Button text='Return home' href='/' type='primary'></Button>
                 </div>
             </div>  
         </div>
