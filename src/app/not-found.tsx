13302c51c8463e4dbd71322254a164c255e68418
sahith <78869400+sahith-p@users.noreply.github.com>
2024-06-30 13:02:40 -0700

    fix errors

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,14 +8,13 @@ export default function ErrorScreen({error}: ErrorProps) {
     return (
         <div className=''>
         <div className='flex h-[75vh] md:h-[90vh] w-full justify-center items-center'>
-            <div className='grid grid-rows-4'>
-                <span className='text-2xl text-gray-700 text-center'>We&apos;re sorry, an error has occured.</span>
-                <span className='text-xl text-gray-500 text-center'>Please try again later.</span>
-                <span className='text-md text-red-500 text-center'>{error.message}</span>
+            <div className='grid grid-rows-3 mx-6'>
+                <span className='text-6xl text-gray-700 text-center pb-5'>404</span>
+                <span className='text-xl text-gray-500 text-center'>We couldn&apos;t find the page you were looking for.</span>
                 <div className='h-full flex mb-5 items-center w-full justify-center'>
                     <Button text='Return home' href='/home' type='primary'></Button>
                 </div>
-            </div>
+            </div>  
         </div>
         <div className='w-full flex items-center justify-center'>
             <Image
--- src/components/dashboard/UserMenu.tsx
+++ src/components/dashboard/UserMenu.tsx
@@ -11,7 +11,7 @@ export default function UserMenu({user}: UserMenuProps) {
     <div className="absolute top-0 right-0 mt-4 mr-4 border-2 p-3 rounded-full shadow-md">
       <div className="flex items-center cursor-pointer">
         <Image src={user.picture as string} alt={user.name as string} className="w-8 h-8 rounded-full mr-2" width={10} height={10}/>
-        <NavItem name={user.nickname || user.name as string} childrenItems={[{name:"Logout", href: "/api/auth/logout"}, {name:"Home", href: "/home"}]}/>
+        <NavItem dropdown={true} name={user.nickname || user.name as string} childrenItems={[{name:"Logout", href: "/api/auth/logout"}, {name:"Home", href: "/home"}]}/>
       </div>
     </div>
   );
